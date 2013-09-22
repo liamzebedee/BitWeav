4f3fb789db45836e90a193a55de07e22ceb31086
Liam Edwards-Playne <liamzebedee@yahoo.com.au>
2013-09-22 19:14:44 +1000

    Remove the old stylesheets

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-var gui = require('nw.gui');
-var main_window = gui.Window.get();
-
+// Generate and link the stylesheet.
+// ---------------------------------
 var less = require('less');
 var fs = require('fs');
-compiled_style_path = "css/.main.compiled-" + new Date().getTime() + ".css";
+compiled_style_name = ".main.compiled-" + new Date().getTime() + ".css";
+compiled_style_path = "css/" + compiled_style_name;
 fs.readFile('css/main.less', function ( error, data ) {
   less.render(data.toString(), function (e, css) {
     if(e !== null) { console.log("\nLESS error in css/main.less: \n\t"+e); }
@@ -19,12 +19,29 @@ fs.readFile('css/main.less', function ( error, data ) {
 });
 
 
+// Delete any old generated stylesheets.
+// -------------------------------------
+fs.readdir('./css', function(error, files) {
+  if (error) throw error;
+  files.filter(function (fileName) {
+    if(fileName === compiled_style_name) return false;
+    else return /.main.compiled-[0-9]*.css/.test(fileName);
+  }).forEach(function(filename){ fs.unlinkSync("./css/"+filename, function(err){console.log(err);}); });
+});
+
+
+// Show and maximise the window.
+// -----------------------------
+var gui = require('nw.gui');
+var main_window = gui.Window.get();
 window.onload = function() {
   main_window.maximize();
   main_window.show();
 }
 
 
+// Initialise AngularJS
+// --------------------
 angular.module('BitWeav', []).
 // Configuration.
 config(['$routeProvider', function($routeProvider) {
