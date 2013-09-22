53c494945bab7eee6755d009a502e3267f7eb72f
Liam Edwards-Playne <liamzebedee@yahoo.com.au>
2013-09-22 18:13:19 +1000

    The CSS loading issue is progressing, now waits and loads well

--- js/app.js
+++ js/app.js
@@ -1,6 +1,20 @@
 var gui = require('nw.gui');
 var main_window = gui.Window.get();
-onload = function() {
+
+window.onload = function() {
+  var less = require('less');
+  var fs = require('fs');
+  compiled_style_path = "css/.main.compiled-" + new Date().getTime() + ".css";
+  fs.readFile('css/main.less', function ( error, data ) {
+    less.render(data.toString(), function (e, css) {
+      if(e !== null) { console.log("\nLESS error in css/main.less: \n\t"+e); }
+      fs.writeFile(compiled_style_path, css, function(err) {
+        $('head').append('<link rel="stylesheet" type="text/css" href="' + compiled_style_path + '?v=' + new Date().getTime() + '"/>');
+        if(err) { console.log(err); }
+      });
+    });
+  });
+
   main_window.maximize();
   main_window.show();
   //main_window.showDevTools();
--- js/setup.js
+++ js/setup.js
@@ -1,20 +1,3 @@
 /*
  * Does stuff before application startup
  */
-
-
-/// Compile the LESS CSS in css/main.less
-/// (we do this here so the user sees a nicely styled page on startup)
-///
-var less = require('less');
-var fs = require('fs');
-
-fs.unlink('css/.main.compiled.less', function(a,b){});
-fs.readFile('css/main.less', function ( error, data ) {
-  less.render(data.toString(), function (e, css) {
-    if(e !== null) { console.log("\nLESS error in css/main.less: \n\t"+e); }
-    fs.writeFile("./css/.main.compiled.css", css, function(err) {
-      if(err) { console.log(err); }
-    });
-  });
-});
