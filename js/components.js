4f024c7f4c313e46e992c656c90891fd4ffd66b4
Liam Edwards-Playne <liamzebedee@yahoo.com.au>
2013-09-26 16:44:31 +1000

    Refactoring, more reply form functionality (yay)

--- js/components.js
+++ js/components.js
@@ -18,11 +18,24 @@ directive('message', function() {
       messageId: '@'
     },
     controller: function($scope, $element, $attrs, $transclude) {
-      $scope.hover = function(){};
       $scope.expand = function($event){
         $scope.expanded = !$scope.expanded;
         $event.stopPropagation();
-      };
+      }
+      $scope.profileCard = function($event) {
+        $scope.showingProfileCard = !$scope.showingProfileCard;
+        $event.stopPropagation();
+      }
+      $scope.composeReply = function($event){
+        $scope.expand($event);
+      }
+      $scope.favourite = function($event){
+        $event.stopPropagation();
+      }
+      $scope.share = function($event){
+        $event.stopPropagation();
+      }
+      $scope.sendReply = function(replyContent){}
     },
     templateUrl: 'partials/templates/message.html',
     replace: true,
