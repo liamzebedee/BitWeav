f3b3e9e2c11c31f17860dad64c8040ac8513967b
Liam Edwards-Playne <liamzebedee@yahoo.com.au>
2013-09-27 22:09:23 +1000

    Removed redundant comments

--- js/filters.js
+++ js/filters.js
@@ -2,11 +2,9 @@ angular.module('BitWeav').
 
 filter('autolink', function() {
   // Modified from original AngularJS linky filter.
-  // This is the single most beautiful piece of code I have ever written.
-  // In something so complex lies the beautiful knowledge that you understand it.
   // This will match URLS of protocols HTTP, HTTPS, FTP, Tent.io, Mailto, and Magnet.
+  // Also matches hashtags.
   var /*THE_GOD_EXPRESSION*/URL_HASHTAG_REGEX = /(((tent\+https?|ftp|https?|irc):\/\/|bitcoin|magnet:\|mailto:)|[A-Za-z0-9._%+-]+@)\S*[^\s\.\;\,\(\)\{\}\<\>]|\S*#(?:\[[^\]]+\]|\S+)/g;
-  //var /*THE_GOD_EXPRESSION*/URL_HASHTAG_REGEX = /(((tent\+https?|ftp|https?|irc):\/\/|bitcoin|magnet:\?|mailto:)|[A-Za-z0-9._%+-]+@)\S*[^\s\.\;\,\(\)\{\}\<\>]/;
 
   return function(text, target) {
     var html = text;
@@ -20,7 +18,7 @@ filter('autolink', function() {
       }
       // Link
       else {
-        html = html.replace(match, "<a open-in-browser href='" + match + "'>"+match+"</a>");
+        html = html.replace(match, "<a href='" + match + "'>"+match+"</a>");
       }
     }
     return html;
